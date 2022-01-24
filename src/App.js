dfb4d8379d1f87cc308fc564aa383a313b0fa95f
DevrMichael <88589247+DevrMichael@users.noreply.github.com>
2022-01-24 21:20:54 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import JSONDATA from "./MOCK_DATA.json";
+import { useState } from "react";
 
 function App() {
+  const [ searchTitle, setSearchTitle ] = useState('');
+  const [ searchRank, setSearchRank ] = useState('');
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>IMDB's Top 100 Movies in 2022</h1>
+      <input
+        type="text"
+        placeholder="Search Title..."
+        onChange={(event) => {
+          setSearchTitle(event.target.value);
+        }}
+      />
+      <h2>Or</h2>
+      <input
+        type="text"
+        placeholder="Search Rank..."
+        onChange={(event) => {
+          setSearchRank(event.target.value);
+        }}
+      />
+
+      {JSONDATA.filter((val) => {
+        if (searchTitle == '') {
+          return null;
+        } else if (val.title.toLowerCase().includes(searchTitle.toLowerCase())) {
+          return val;
+        }
+      }).map((val, key) => {
+        return (
+          <div className="movieTitle" key={key}>
+            <h3 className="title">{val.title}</h3>
+            <p className="rank">Rank: {val.rank}</p>
+            <hr />
+          </div>
+        );
+      })}
+
+      {JSONDATA.filter((val) => {
+        if (searchRank == '') {
+          return null;
+        } else if (val.rank.includes(searchRank)) {
+          return val;
+        }
+      }).map((val, key) => {
+        return (
+          <div className="movieTitle" key={key}>
+            <h3 className="title">{val.title}</h3>
+            <p className="rank">Rank: {val.rank}</p>
+            <hr />
+          </div>
+        );
+      })}
     </div>
   );
 }
